b9c83c5cbfebd779d075a3be038f0c0586132cd8
Maciej Topolewski <maciej_topolewski@epam.com>
2022-10-17 17:00:14 +0200

    add path check logic

--- data_io.py
+++ data_io.py
@@ -1,3 +1,5 @@
+import os
+
 from json import dump
 
 import pandas as pd
@@ -6,24 +8,29 @@ import pandas as pd
 # Input
 def take_input_data(path: str) -> pd.DataFrame:
 
-    file = pd.read_csv(
-        path,
-        on_bad_lines="skip",
-        sep=" ",
-        skipinitialspace=True,
-        names=[
-            "Timestamp in seconds since the epoch",
-            "Response header size in bytes",
-            "Client IP address",
-            "HTTP response code",
-            "Response size in bytes",
-            "HTTP request method",
-            "URL",
-            "Username",
-            "Type of access/destination IP address",
-            "Response type",
-        ],
-    )
+    if os.path.isfile(path):
+        file = pd.read_csv(
+            path,
+            on_bad_lines="skip",
+            sep=" ",
+            skipinitialspace=True,
+            names=[
+                "Timestamp in seconds since the epoch",
+                "Response header size in bytes",
+                "Client IP address",
+                "HTTP response code",
+                "Response size in bytes",
+                "HTTP request method",
+                "URL",
+                "Username",
+                "Type of access/destination IP address",
+                "Response type",
+            ],
+        )
+    elif os.path.isdir(path):
+        pass # TODO: add a directory concatenate script
+    else:
+        raise TypeError # TODO: add a specific message
     df = pd.DataFrame(data=file)
     return df
 
