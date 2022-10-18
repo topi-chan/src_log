7af04cf571e1ec43b4997381ece748e53b4b4df4
Maciej Topolewski <maciej_topolewski@epam.com>
2022-10-18 13:50:27 +0200

    major console output improvements

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
+import sys
 
 import click
 from rich.console import Console
 
-from data_io import save_results, load_input_data
+from data_io import load_input_data, save_results
 from operations import bytes_total, e_ps, lf_ip, mf_ip
 
 console = Console()
 
 
+if sys.argv[1] == "--help":
+    console.print(
+        """Hi :vampire:\n[b][i]This is a console log analyzer program[/i]\nIs has four operations.
+        \nIt takes an operation type argument, then location of file or a directory with logs, then directory to save result
+        \nTo use it run a command for a operation as specified below."""
+    )
+
+
 @click.group()
 def analyze_logs():
     pass
-
-
-console.print(
-    """Hi :vampire:\n[b][i]This is a console log analyzer program[/i]\nIs has four operations.
-    \nIt takes an operation type argument, then location of file or a directory with logs, then directory to save result
-    \nTo use it run a command for a operation as specified below."""
-)
 
 
 @analyze_logs.command(name="mfip")
@@ -36,6 +38,8 @@ def mfip(mfip, input, output):
     df = load_input_data(input)
     mf = mf_ip(df)
     save_results("Most frequent IP", mf, output)
+    console.print(
+        f"\n\ni][b][bold magenta]Most frequent IP determined and saved in: {output}.[/bold magenta][b][i]")
 
 
 @analyze_logs.command(name="lfip")
@@ -53,6 +57,8 @@ def lfip(lfip, input, output):
     df = load_input_data(input)
     lf = lf_ip(df)
     save_results("Least frequent IP", lf, output)
+    console.print(
+        f"\n\n[i][b][bold magenta]Least frequent IP determined and saved in: {output}.[/bold magenta][b][i]")
 
 
 @analyze_logs.command(name="eps")
@@ -70,6 +76,8 @@ def eps(eps, input, output):
     df = load_input_data(input)
     eps = e_ps(df)
     save_results("Events per second", eps, output)
+    console.print(
+        f"\n\n[i][b][bold magenta]Events per second counted and saved in: {output}.[/bold magenta][b][i]")
 
 
 @analyze_logs.command(name="bytes")
@@ -87,6 +95,8 @@ def bytes(bytes, input, output):
     df = load_input_data(input)
     bytes = bytes_total(df)
     save_results("Total amount of bytes exchanged", bytes, output)
+    console.print(
+        f"\n\n[i][b][bold magenta]Total amount of bytes exchanged counted and saved in: {output}.[/bold magenta][b][i]")
 
 
 if __name__ == "__main__":
