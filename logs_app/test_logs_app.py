789ceded0418739f67a1def59e01fb80453b3126
Maciej Topolewski <maciej_topolewski@epam.com>
2022-10-20 15:15:51 +0200

    add simple test and readme notice

--- logs_app/test_logs_app.py
+++ logs_app/test_logs_app.py
@@ -1,8 +1,9 @@
 import pandas
 import pytest
 import pandas as pd
-from .data_io import read_table, PD_COLUMN_NAMES
-from .operations import mf_ip
+from .data_io import read_table, PD_COLUMN_NAMES, save_results
+from .operations import mf_ip, lf_ip, e_ps, bytes_total
+#from .main import base_task
 
 
 def test_read_data():
@@ -19,9 +20,19 @@ class TestEnv:
 
     def __init__(self):
         self.test_df = read_table("log_test.txt")
-        self.analyze_result = {"Events per second": "5.476362402868889"}
+        self.analyze_result_eps = {"Events per second": "5.476362402868889"}
 
 
+env = TestEnv()
 
-def test_operations():
-    pass
+
+# def test_base_task():
+#     base_task_df = base_task()
+#     assert base_task_df == env.test_df
+
+def test_compare_ips():
+    lf_value = lf_ip(env.test_df)
+    mf_value = mf_ip(env.test_df)
+
+    assert mf_value != lf_value
+
