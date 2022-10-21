06922bfdd17f3b93a181d495ad3c4104db20c99e
Maciej Topolewski <maciej_topolewski@epam.com>
2022-10-21 12:05:50 +0200

    add a few tests

--- logs_app/test_logs_app.py
+++ logs_app/test_logs_app.py
@@ -1,7 +1,8 @@
 import numpy
 import pandas as pd
+import pytest
 
-from logs_app.data_io import PD_COLUMN_NAMES, read_table, save_results
+from logs_app.data_io import PD_COLUMN_NAMES, load_input_data, read_table, save_results
 from logs_app.operations import bytes_total, e_ps, lf_ip, mf_ip
 
 from .main import base_task
@@ -16,42 +17,48 @@ def test_read_data():
     assert list(test_df.columns) == PD_COLUMN_NAMES  # test exact names
 
 
-class TestEnv:
-    """Create environment to use in tests"""
-
-    def __init__(self):
-        self.test_df = read_table("log_test.txt")
-        self.analyze_result_eps = {"Events per second": "5.476362402868889"}
-
-
-env = TestEnv()
+test_df = read_table("log_test.txt")
 
 
 def test_compare_ips():
-    lf_value = lf_ip(env.test_df)
-    mf_value = mf_ip(env.test_df)
+    lf_value = lf_ip(test_df)
+    mf_value = mf_ip(test_df)
 
     assert type(lf_value) is str and type(mf_value) is str
     assert mf_value != lf_value
 
 
 def test_eps():
-    eps_result = e_ps(env.test_df)
+    eps_result = e_ps(test_df)
 
-    assert env.test_df["date"] is not None
-    assert type(env.test_df["date"]) is pd.core.series.Series
+    assert test_df["date"] is not None
+    assert type(test_df["date"]) is pd.core.series.Series
     assert eps_result is not None and type(eps_result) is float
 
 
 def test_bytes_total():
-    bytes_result = bytes_total(env.test_df)
+    bytes_result = bytes_total(test_df)
 
     assert bytes_result is not None
     assert type(bytes_result) is numpy.int64
 
 
+def test_load_input_data_simple_txt_file():
+    text_input_result = load_input_data("log_test.txt")
+    assert type(text_input_result) is pd.DataFrame
+    assert len(text_input_result) == 100
+    assert len(text_input_result.columns) == 10
+    assert list(text_input_result.columns) == PD_COLUMN_NAMES
+
+
+def test_load_input_data_last_condition():
+    with pytest.raises(TypeError):
+        text_input_result = load_input_data("/wrong_path")
+        assert text_input_result is None
+
+
 def test_save_results():
-    json_result = save_results("Test Col", env.analyze_result_eps, "path")
+    json_result = save_results("Test Col", "str", "path")
     assert json_result is None
 
 
